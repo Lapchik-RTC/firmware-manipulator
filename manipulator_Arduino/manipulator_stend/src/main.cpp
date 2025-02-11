39fefd162ca334a94a0a0af06e0c900ae3a7ccd5
Garik-prog <->
2025-02-11 03:11:36 +0300

    кодим... настроил передачу пакетов...

--- manipulator_Arduino/manipulator_stend/src/main.cpp
+++ manipulator_Arduino/manipulator_stend/src/main.cpp
@@ -176,49 +176,155 @@ void manipulator(byte x1, byte y1) {
 }
 
 
-int v_vpered = 40;
+int v_vpered = 50;
 int v_angle = 70;
-float kp = 2;
+float kp = 0.3;
 float kd = 4;
 static int volatile errOld = 0;
+static int u = 0;
 
-void azimut(int zahvat_x, int res_x) {
+void azimut(int zahvat_x, int zahvat_y, int tag_x, int tag_y) {
     
-    int e = res_x - zahvat_x;
-    int u = int(e*kp + (e - errOld)* kd);
+    int e = tag_x - zahvat_x;
+    u = int(e*kp + (e - errOld)* kd);
+    u = constrain(u, 0, 30);
     errOld = e;
-    motor(1, v_vpered);
-    motor(2, v_angle + u);
-    motor(3, v_angle - u);
+    // int v_vpered = v_angle - u;
+    motor(1, v_vpered + u);
+    motor(2, v_vpered - u - 15);
+    motor(3, v_vpered - u - 25);
+    // motor(1, -40);
+    // motor(2, -40);
+    // motor(3, -40);
+}
+
+struct Data
+{ 
+public:
+  int16_t tag_x;    
+  int16_t tag_x_old;    
+  int16_t tag_y;
+  int16_t zahvat_x;
+  int16_t zahvat_y;
+  int16_t pole_x;
+  int16_t pole_y;
+  bool CRC_Error;
+};
+Data data = {
+  .tag_x      = 0,
+  .tag_x_old  = 1000,
+  .tag_y      = 0,
+  .zahvat_x   = 0,
+  .zahvat_y   = 0,
+  .pole_x     = 0,
+  .pole_y     = 0,
+  .CRC_Error  = false
+};
+
+uint8_t crc8(uint8_t *pcBlock, unsigned int len)
+{
+    unsigned char crc = 0xFF;
+    unsigned int i;
+
+    while (len--)
+    {
+        crc ^= *pcBlock++;
+
+        for (i = 0; i < 8; i++)
+            crc = crc & 0x80 ? (crc << 1) ^ 0x31 : crc << 1;
+    }
+
+    return crc;
+}
+
+Data readInt()
+{
+  while(Serial3.read() != 0xFF);
+  uint8_t res_arr[9] = {0};
+  /*
+  res_arr[0] = tag_lo;   
+  res_arr[1] = tag_hi;   
+  res_arr[2] = tag_y;
+  res_arr[3] = zahvat_lo;
+  res_arr[4] = zahvat_hi;
+  res_arr[5] = zahvat_y;
+  res_arr[6] = pole_lo;
+  res_arr[7] = pole_hi;
+  res_arr[8] = pole_y;
+  */
+  while(Serial3.available() < 10);
+  for(int i = 0; i < 9; i++) {
+    res_arr[i] = Serial3.read();
+    // Serial.print("res_arr["); Serial.print(i); Serial.print("]: "); Serial.println(res_arr[i]);
+  }
+  uint8_t crc_in = Serial3.read();
+  if (crc_in != crc8(res_arr, 9)) {
+    // Serial.print(crc_in);
+    // Serial.print("   ");
+    // Serial.println(crc8(res_arr, 9));
+    Serial.println("CRC error!!!");
+    // delay(2000);
+    data.CRC_Error = true;
+    return data;
+  }
+  else {
+    data.tag_x = (res_arr[1] << 8) | res_arr[0];
+    data.tag_y = res_arr[2];
+    data.zahvat_x = (res_arr[4] << 8) | res_arr[3];
+    data.zahvat_y = res_arr[5];
+    data.pole_x = (res_arr[7] << 8) | res_arr[6];
+    data.pole_y = res_arr[8];
+    data.CRC_Error = false;
+    // Serial.println("CRC success!!!");
+    return data;
+  }
+}
+
+void testSerRead()
+{
+  Serial.println(Serial3.read());
+}
+
+void printData(const Data& d) {
+  Serial.print("  tag_x: "); Serial.print(d.tag_x);
+  Serial.print("  tag_x_old: "); Serial.print(d.tag_x_old);
+  Serial.print("  tag_y: "); Serial.print(d.tag_y);
+  Serial.print("  zahvat_x: "); Serial.print(d.zahvat_x);
+  Serial.print("  zahvat_y: "); Serial.print(d.zahvat_y);
+  Serial.print("  pole_x: "); Serial.print(d.pole_x);
+  Serial.print("  pole_y: "); Serial.print(d.pole_y);
+  Serial.print("  CRC_Error: "); Serial.print(d.CRC_Error ? "true " : "false ");
+  Serial.print("  u: "); Serial.println(u);
+}
+
+void stop() {
+  motor(1, 0);
+  motor(2, 0);
+  motor(3, 0);
 }
 
 void loop() {
-    int zahvat = 0;
-    int res = 0;
-    int i1 = 0;
-    int i2 = 0;
-    if (Serial3.available()) {
-        i1 = Serial3.read();
-        // Serial.println(tg);
+    Data d = readInt();
+    printData(d);
+    if (true) {
+      d.tag_x_old = d.tag_x;
     }
-    if (Serial3.available()) {
-        i2 = Serial3.read();
-    }
-    if (i1 != 0 && i2 != 0) { 
-        if (i1%2 == 0) {
-            res = i1;
-            zahvat = i2;
+    if (!d.CRC_Error) {
+      if (d.zahvat_x < d.tag_x_old)
+      {
+        d = readInt();  
+        if (d.tag_x != 0) {
+          d.tag_x_old = d.tag_x;
         }
-        else {
-            res = i2;
-            zahvat = i1;
-        }
+        azimut(d.zahvat_x, d.zahvat_y, d.tag_x, d.tag_y);
+        printData(d);
+      }
+      else {
+        stop();
+      }
+      
     }
-    if (res == 0 || zahvat != 0) { 
-        Serial.print(res);
-        Serial.print("\t");
-        Serial.println(zahvat);
-    }
-    
-    
+    // printData(d);
+    delay(500);
+    // testSerRead();
 }
